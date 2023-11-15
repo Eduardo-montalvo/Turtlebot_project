4eeed1cd2d1509d12c8fedb8f17769a8fcb5dcad
Eduardo-montalvo <eduardo.montalvo12@hotmail.com>
2023-11-15 04:15:07 -0600

    Implementation of the node of motion

--- build/robot_movement/build/lib/robot_movement/node_movement.py
+++ build/robot_movement/build/lib/robot_movement/node_movement.py
@@ -7,27 +7,29 @@ class MyNode(Node):
     def __init__(self):
         super().__init__("Movement_publisher")
         self.publisher =self.create_publisher(Twist,'/cmd_vel', 10)
-        timer_period = 2 # 0.5 hz
-        self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.counter = 0
+        self.timer_ = self.create_timer(1.0, self.timer_callback)
       
     def timer_callback(self):
         msg=Twist()
-        msg.linear.x = -50 #Aqui va el valor al que vamos a meter
-        msg.linear.y = 0 #Aqui va el valor al que vamos a meter
-        msg.linear.z = 0 #Aqui va el valor al que vamos a meter
+        msg.linear.x = 0.8 #Aqui va el valor al que vamos a meter
+        msg.linear.y = 0.0 #Aqui va el valor al que vamos a meter
+        msg.linear.z = 0.0 #Aqui va el valor al que vamos a meter
 
-        msg.angular.x = 0 #Aqui va el valor al que vamos a meter
-        msg.angular.y = 0 #Aqui va el valor al que vamos a meter
-        msg.angular.z = 0 #Aqui va el valor al que vamos a meter
+        msg.angular.x = 0.0 #Aqui va el valor al que vamos a meter
+        msg.angular.y = 0.0 #Aqui va el valor al que vamos a meter
+        msg.angular.z = 0.0 #Aqui va el valor al que vamos a meter
         self.publisher.publish(msg) #We publish the distance with this command
+        self.get_logger().info('Publicar velocidad: {}'.format(msg))
 
 
 def main(args=None):
     rclpy.init(args=args)#First line 
     #Here comes the code
     node = MyNode()
-    rclpy.spin(node)
-    rclpy.shutdown() #Last line 
+    try:
+        rclpy.spin(node)
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
 if __name__ == "__main__":
     main()
